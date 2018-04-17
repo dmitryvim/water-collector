325a81664ea9fac1e32c53055453f0ac1c0ec2b0
dmitryvim <dmitry.mikhailovich@gmail.com>
2018-04-17 23:02:20 +0300

    init maven app

---
+++ src/main/java/com/mikhaylovich/watercollector/WaterCollectorApplication.java
@@ -0,0 +1,10 @@
+package com.mikhaylovich.watercollector;
+
+/**
+ * Created by dmitry on 17.04.2018. ${PATH}
+ */
+public class WaterCollectorApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
